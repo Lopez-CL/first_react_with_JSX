c450c8f8ce8d0699862685979c9d89f34cee03d5
Lopez-CL <cllopez511@gmail.com>
2022-10-26 20:48:26 -0400

    finishing assignment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = ()  => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt Everest</li>
+          <li>Run a Marathon</li>
+          {/* What an ableist list... */}
+          <li>Feed the cats!</li>
+        </ul>
+    </>
   );
 }
 
